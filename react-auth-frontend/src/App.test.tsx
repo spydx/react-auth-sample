91147f931d4e3ce883c0db0acf71b934d13648bc
Kenneth <fossen.kenneth@gmail.com>
2021-04-03 17:11:08 +0200

    React test and CI

--- react-auth-frontend/src/App.test.tsx
+++ react-auth-frontend/src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from './App';
+import {store} from './state/store';
+import { Provider } from 'react-redux';
+import { create} from 'react-test-renderer';
+
+describe('A snapshot test of App', () => {
+  test('Render App', () => {
+    let tree = create(<Provider store={store}><App /></Provider>)
+    expect(tree.toJSON()).toMatchSnapshot();
+  })
+})
